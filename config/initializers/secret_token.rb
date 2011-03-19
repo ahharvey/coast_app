250f725efed1a4298a99bd58ae89c6cf64ed80d6
System Administrator <root@andrew-harveys-macbook-pro.local>
2011-03-19 09:59:56 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoastApp::Application.config.secret_token = '5ed6c14cd243e9341a86b3ef1674ac31ddf13558ada2977cc3787986439db203c647f84d423a9769c0527d45d3d99edf7f56392cda5efb818a7d5a6455c2b6b4'
